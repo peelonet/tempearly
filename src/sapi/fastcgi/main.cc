df5b639be7beaccd8b8872d31d5c1c24cb8099f7
Rauli L <rauli.laine@gmail.com>
2014-10-11 18:12:43 +0300

    Make sure that the response is always committed.

--- src/sapi/fastcgi/main.cc
+++ src/sapi/fastcgi/main.cc
@@ -27,6 +27,10 @@ int main(int argc, char** argv)
             {
                 interpreter->response->SendException(interpreter->GetException());
             }
+            else if (!interpreter->response->IsCommitted())
+            {
+                interpreter->response->Commit();
+            }
         }
     }
 
