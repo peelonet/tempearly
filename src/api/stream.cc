0bef7100f81c5c226ef281804bd48f201e3f6f32
Rauli Laine <rauli.laine@gmail.com>
2014-12-01 20:06:03 +0200

    API: Stream improvements.
    1. Adds `readline` method to `Stream` class which reads single line from
       the stream and returns it.
    2. Modifies `Stream` class so that it inherits from `Iterable` and has
       an iterator which iterates over each line in the stream.
    3. Modifies `File.open` method so that it takes an optional function as
       argument.

--- src/api/stream.cc
+++ src/api/stream.cc
@@ -1,4 +1,7 @@
 #include "interpreter.h"
+#include "api/iterator.h"
+#include "core/bytestring.h"
+#include "core/stringbuilder.h"
 
 namespace tempearly
 {
@@ -21,6 +24,8 @@ namespace tempearly
      *
      * If the amount is null, entire contents of the stream are read.
      *
+     * If end of input input is reached, null is returned instead.
+     *
      * Throws: IOError - If stream is not readable or if any IO related error
      * occurs while reading.
      */
@@ -29,6 +34,82 @@ namespace tempearly
         interpreter->Throw(interpreter->eIOError, "Stream is not readable");
 
         return Value();
+    }
+
+    /**
+     * Stream#readline() => Binary or String
+     *
+     * Reads single line (separated either by \r\n, \n or just \r) from the
+     * stream and returns it as an Binary or String object, depending on
+     * whether the stream reads binary data or not.
+     *
+     * If end of input is reached, null is returned instead.
+     *
+     * Throws: IOError - If stream is not readable or if any IO related error
+     * occurs while reading.
+     */
+    TEMPEARLY_NATIVE_METHOD(stream_readline)
+    {
+        const Value& receiver = args[0];
+        Vector<Value> one(1, Value::NewInt(1));
+        Value result = receiver.Call(interpreter, "read", one);
+
+        if (!result)
+        {
+            return Value();
+        }
+        else if (result.IsBinary())
+        {
+            Vector<byte> buffer;
+            ByteString bytes = result.AsBinary();
+
+            while (!bytes.IsEmpty() && bytes.GetBack() != '\n')
+            {
+                buffer.PushBack(bytes.GetBytes(), bytes.GetLength());
+                if (!(result = receiver.Call(interpreter, "read", one)))
+                {
+                    return Value();
+                }
+                else if (!result.IsBinary())
+                {
+                    break;
+                }
+                bytes = result.AsBinary();
+            }
+            if (!buffer.IsEmpty() && buffer.GetBack() == '\r')
+            {
+                buffer.Erase(buffer.GetSize() - 1);
+            }
+
+            return Value::NewBinary(ByteString(buffer.GetData(), buffer.GetSize()));
+        }
+        else if (result.IsString())
+        {
+            StringBuilder buffer;
+            String runes = result.AsString();
+
+            while (!runes.IsEmpty() && runes.GetBack() != '\n')
+            {
+                buffer.Append(runes);
+                if (!(result = receiver.Call(interpreter, "read", one)))
+                {
+                    return Value();
+                }
+                else if (!result.IsString())
+                {
+                    break;
+                }
+                runes = result.AsString();
+            }
+            if (!buffer.IsEmpty() && buffer.GetBack() == '\r')
+            {
+                buffer.Erase(buffer.GetLength() - 1);
+            }
+
+            return Value::NewString(buffer.ToString());
+        } else {
+            return Value::NullValue();
+        }
     }
 
     /**
@@ -75,17 +156,72 @@ namespace tempearly
         return Value::NullValue();
     }
 
+    namespace
+    {
+        class StreamIterator : public IteratorObject
+        {
+        public:
+            explicit StreamIterator(const Handle<Interpreter>& interpreter, const Value& stream)
+                : IteratorObject(interpreter->cIterator)
+                , m_stream(stream) {}
+
+            Result Generate(const Handle<Interpreter>& interpreter)
+            {
+                if (m_stream)
+                {
+                    Value line = m_stream.Call(interpreter, "readline");
+
+                    if (!line)
+                    {
+                        return Result(Result::KIND_ERROR);
+                    }
+                    else if (!line.IsNull())
+                    {
+                        return Result(line);
+                    }
+                    m_stream.Clear();
+                }
+
+                return Result(Result::KIND_BREAK);
+            }
+
+            void Mark()
+            {
+                IteratorObject::Mark();
+                m_stream.Mark();
+            }
+
+        private:
+            Value m_stream;
+            TEMPEARLY_DISALLOW_COPY_AND_ASSIGN(StreamIterator);
+        };
+    }
+
+    /**
+     * Stream#__iter__() => Iterator
+     *
+     * Returns an iterator which iterates over each line in the stream, using
+     * "readline" method.
+     */
+    TEMPEARLY_NATIVE_METHOD(stream_iter)
+    {
+        return Value(new StreamIterator(interpreter, args[0]));
+    }
+
     void init_stream(Interpreter* i)
     {
-        Handle<Class> cStream = i->AddClass("Stream", i->cObject);
+        Handle<Class> cStream = i->AddClass("Stream", i->cIterable);
 
         i->cStream = cStream.Get();
 
         cStream->AddMethod(i, "close", 0, stream_close);
         cStream->AddMethod(i, "read", -1, stream_read);
+        cStream->AddMethod(i, "readline", 0, stream_readline);
         cStream->AddMethod(i, "write", -1, stream_write);
 
         cStream->AddMethod(i, "print", -1, stream_print);
+
+        cStream->AddMethod(i, "__iter__", 0, stream_iter);
 
         cStream->AddMethodAlias(i, "<<", "print");
     }
