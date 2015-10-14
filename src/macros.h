829b1844b5a5d3e151d628b465bf56aa7008491d
Rauli Laine <rauli.laine@iki.fi>
2015-10-14 23:10:13 +0300

    Require C++11 compliant compiler.
    I believe that the time has come for us to require C++11 compliant
    compiler for this project. C++11 support (at least partial support for
    it) can now be found in all major C++ compilers and to (ab)use features
    given by the "new" version of C++, we need to require an compiler that
    has support for it.

--- src/macros.h
+++ src/macros.h
@@ -1,21 +1,13 @@
 #ifndef TEMPEARLY_MACROS_H_GUARD
 #define TEMPEARLY_MACROS_H_GUARD
 
-#if __cplusplus > 199711L
-# define TEMPEARLY_DISALLOW_COPY_AND_ASSIGN(TypeName) \
-    TypeName(const TypeName&) = delete; \
-    void operator=(const TypeName&) = delete
-# define TEMPEARLY_DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
+#define TEMPEARLY_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+    TypeName(const TypeName &) = delete; \
+    void operator=(const TypeName &) = delete
+
+#define TEMPEARLY_DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
     TypeName() = delete; \
-    TEMPERALY_DISALLOW_COPY_AND_ASSIGN(TypeName)
-#else
-# define TEMPEARLY_DISALLOW_COPY_AND_ASSIGN(TypeName) \
-    TypeName(const TypeName&); \
-    void operator=(const TypeName&)
-# define TEMPEARLY_DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
-    TypeName(); \
     TEMPEARLY_DISALLOW_COPY_AND_ASSIGN(TypeName)
-#endif
 
 #define TEMPEARLY_NATIVE_METHOD(MethodName) \
     static void MethodName(const Handle<Interpreter>& interpreter, \
